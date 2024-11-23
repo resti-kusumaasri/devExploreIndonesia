f627d9cdc088cd61926840aec6dd4edda42c4830
Erfin-FlyingCow <noufalirfan02@gmail.com>
2024-11-23 20:03:03 +0700

    adjustment

--- app/src/main/java/com/example/exploreindonesia/AuthActivity.kt
+++ app/src/main/java/com/example/exploreindonesia/AuthActivity.kt
@@ -1,7 +1,9 @@
 package com.example.exploreindonesia
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import com.example.exploreindonesia.databinding.ActivityAuthBinding
 import com.example.exploreindonesia.ui.auth_ui.LoginFragment
@@ -12,7 +14,10 @@ class AuthActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAuthBinding
     private lateinit var btnlogin: ImageView
     private lateinit var btnregister: ImageView
+    private lateinit var btnsubmit: Button
 
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
@@ -25,10 +30,12 @@ class AuthActivity : AppCompatActivity() {
         binding = ActivityAuthBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        btnlogin = findViewById(R.id.btn_login)
-        btnregister = findViewById(R.id.btn_register)
+        btnlogin = binding.btnLogin
+        btnregister = binding.btnRegister
+        btnsubmit = binding.btnSubmit
 
         btnregister.setOnClickListener {
+            btnsubmit.setText(R.string.register)
             btnlogin.setImageResource(R.drawable.btn_login_inactive)
             btnregister.setImageResource(R.drawable.btn_register_active)
             supportFragmentManager.beginTransaction()
@@ -37,6 +44,7 @@ class AuthActivity : AppCompatActivity() {
         }
 
         btnlogin.setOnClickListener {
+            btnsubmit.setText(R.string.login)
             btnlogin.setImageResource(R.drawable.btn_login_active)
             btnregister.setImageResource(R.drawable.btn_register_inactive)
             supportFragmentManager.beginTransaction()
