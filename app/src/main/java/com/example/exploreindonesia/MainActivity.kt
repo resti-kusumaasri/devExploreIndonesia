dd08b747d88645c873d81c113b6c80333db2b5a8
Erfin-FlyingCow <noufalirfan02@gmail.com>
2024-11-25 17:49:06 +0700

    add UI Home fragment

--- app/src/main/java/com/example/exploreindonesia/MainActivity.kt
+++ app/src/main/java/com/example/exploreindonesia/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.exploreindonesia
 
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
@@ -11,6 +13,10 @@ import androidx.navigation.ui.setupWithNavController
 import com.example.exploreindonesia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var button_home_first: LinearLayout
+    lateinit var button_home_second: LinearLayout
+    lateinit var button_home_third: LinearLayout
 
     private lateinit var binding: ActivityMainBinding
 
@@ -23,6 +29,14 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = binding.navView
 
         supportActionBar?.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.actionbarcorner))
+
+        button_home_first = findViewById(R.id.button_home_first)
+        button_home_second = findViewById(R.id.button_home_second)
+        button_home_third = findViewById(R.id.button_home_third)
+
+        button_home_first.setOnClickListener {
+            Toast.makeText(this, "Home First", Toast.LENGTH_SHORT).show()
+        }
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
